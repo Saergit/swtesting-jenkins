a84c2e7d67c6217aa92fdb9c790c59dc1d810b9e
Saergit <petu.heinonen@gmail.com>
2017-11-19 16:09:19 +0200

    committen

---
+++ teht7.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tehtava7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double brutto = 0;
+            int lapset = 0;
+
+            Console.WriteLine("Syötä lapsien lukumäärä: ");
+            lapset = int.Parse(Console.ReadLine());
+            Console.WriteLine("Syötä bruttotulo: ");
+            brutto = double.Parse(Console.ReadLine());
+
+            double vahennykset = 330 + ((double)lapset * 16);
+
+            double ennenveroja = bruttotulo - vahennykset;
+
+            double verot = 0;
+            double netto = 0;
+            if (ennenveroja > 20000)
+            {
+                verot = 0.3 * ennenveroja;
+            }
+            else
+            {
+                verot = 0.2 * ennenveroja;
+            }
+            
+            netto = ennenveroja - verot;
+
+            Console.WriteLine("Bruttotulosi on: ");
+            Console.WriteLine("Vähennyksien määrä: );
+            Console.WriteLine("Verojen osuus: ");
+            Console.WriteLine("Nettotulosi on: ");
+        }
+    }
+}
